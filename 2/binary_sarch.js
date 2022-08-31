d51c656852c88d80167af484a580276a61997bb6
RimmaBuzuluk <rimmysya2000@gmail.com>
2022-08-31 23:43:30 +0300

    binarySearch

--- 2/binary_sarch.js
+++ 2/binary_sarch.js
@@ -28,5 +28,22 @@ function binarySearch(array, item) {
   }
   return position;
 }
-console.log(binarySearch(array, 0));
+
+function recursiveBinarySearch(array, item, start, end) {
+  let middle = Math.floor((start + end) / 2);
+  count += 1;
+  if (item === array[middle]) {
+    return middle;
+  }
+  if (item < array[middle]) {
+    return recursiveBinarySearch(array, item, 0, middle - 1);
+  } else {
+    return recursiveBinarySearch(array, item, middle + 1, end);
+  }
+}
+
+console.log("binarySearch:", binarySearch(array, 8));
+console.log(count);
+
+console.log(recursiveBinarySearch(array, 0, 0, array.length));
 console.log(count);
